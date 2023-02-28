4d3cf874e3901e44e828c07aab67b98e2e685d59
Miguel Teixeira <up202005208@edu.fe.up.pt>
2023-02-28 08:46:21 +0000

    tp2-1

---
+++ tp2/MyInterface.js
@@ -0,0 +1,28 @@
+import {CGFinterface, dat} from '../lib/CGF.js';
+
+/**
+* MyInterface
+* @constructor
+*/
+export class MyInterface extends CGFinterface {
+    constructor() {
+        super();
+    }
+
+    init(application) {
+        // call CGFinterface init
+        super.init(application);
+        
+        // init GUI. For more information on the methods, check:
+        // https://github.com/dataarts/dat.gui/blob/master/API.md
+        this.gui = new dat.GUI();
+
+        //Checkbox element in GUI
+        this.gui.add(this.scene, 'displayAxis').name('Display Axis');//tem de ter true of false
+
+        
+        this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');//tem de ser um numero de 0.1 a 5
+
+        return true;
+    }
+}
--- tp2/README.md
+++ tp2/README.md
@@ -1,1 +1,10 @@
-# [COPY FROM PREVIOUS TP AND ADJUST]
+# CG 2022/2023
+
+## Group T06G010
+
+## TP 1 Notes
+
+- We did all the work required without any major setbacks
+
+
+![Screenshot 1](screenshots/cg-t06g10-tp1-1.png)
